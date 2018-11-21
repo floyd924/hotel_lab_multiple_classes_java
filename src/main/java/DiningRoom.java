90cf0c599b9875c3a1782757945784474d38b494
Genna Walsh <gennawalsh@gmail.com>
2018-11-21 11:56:10 +0000

    add DiningRoom class and tests

--- src/main/java/DiningRoom.java
+++ src/main/java/DiningRoom.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class DiningRoom {
+
+    private int capacity;
+    private String name;
+    private ArrayList<Guest> guestList;
+
+    public DiningRoom(String name) {
+        this.capacity = 5;
+        this.name = name;
+        this.guestList = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Guest> getGuestList() {
+        return this.guestList;
+    }
+
+    public int numberOfGuests() {
+        return this.guestList.size();
+    }
+
+    public void addGuest(Guest guest) {
+        if (numberOfGuests() < this.capacity) {
+            this.guestList.add(guest);
+        }
+    }
+
+    public Guest removeGuest() {
+        if (this.numberOfGuests() > 0) {
+            return this.guestList.remove(0);
+        }
+        return null;
+    }
+
+    public void emptyRoom() {
+        this.guestList.clear();
+    }
+
 }
--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -1,2 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiningRoomTest {
+
+    DiningRoom diningRoom;
+    Guest guest;
+
+    @Before
+    public void setUp() {
+        diningRoom = new DiningRoom("Fuzzy");
+        guest = new Guest();
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(5, diningRoom.getCapacity());
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fuzzy", diningRoom.getName());
+    }
+
+    @Test
+    public void hasGuestList() {
+        assertEquals(0, diningRoom.numberOfGuests());
+    }
+
+    @Test
+    public void canAddGuest() {
+        diningRoom.addGuest(guest);
+        assertEquals(1, diningRoom.numberOfGuests());
+    }
+
+    @Test
+    public void canRemoveGuest() {
+        diningRoom.addGuest(guest);
+        diningRoom.removeGuest();
+        assertEquals(0, diningRoom.numberOfGuests());
+    }
+
+    @Test
+    public void canEmptyRoom() {
+        diningRoom.addGuest(guest);
+        diningRoom.emptyRoom();
+        assertEquals(0, diningRoom.numberOfGuests());
+    }
+
 }
